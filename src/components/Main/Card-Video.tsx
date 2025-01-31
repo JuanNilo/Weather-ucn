94feb25f23fd3b8db3dd0544f5ab2ee49cb83d0d
JuanNilo <juan.nilo@alumnos.ucn.cl>
2025-01-31 11:37:46 -0300

    FIX 3: La venganza de los Sith

--- src/components/Main/Card-Video.tsx
+++ src/components/Main/Card-Video.tsx
@@ -1,22 +1,21 @@
 interface CardVideoProps {
     link: string;
 }
-function CardVideo({ link }: CardVideoProps) {
 
+function CardVideo({ link }: CardVideoProps) {
     return (
         <div className="bg-slate-100 w-[80%] shadow-md border-[1px] border-gray-300 text-black rounded-lg overflow-hidden">
-            <div className="w-full h-full overflow-hidden">
+            <div className="w-full h-0 pb-[56.25%] relative overflow-hidden">
                 <iframe
                     src={`${link}?autoplay=1&controls=0&disablekb=1&modestbranding=1&rel=0&showinfo=0`}
                     frameBorder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                     allowFullScreen
-                    className="w-full h-full pointer-events-none"
+                    className="absolute top-0 left-0 w-full h-full"
                 ></iframe>
             </div>
         </div>
     );
 }
-
 
 export default CardVideo;
