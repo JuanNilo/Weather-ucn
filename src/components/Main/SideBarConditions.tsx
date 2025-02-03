09df3e3fd02daa184fbf2749c8e6a28c5b571362
JuanNilo <juan.nilo@alumnos.ucn.cl>
2025-02-03 15:42:32 -0300

    UPDATE: Temperatura

--- src/components/Main/SideBarConditions.tsx
+++ src/components/Main/SideBarConditions.tsx
@@ -1,6 +1,7 @@
 
 import { useEffect, useState } from 'react'
 function SideBarConditions() {
+    const [temperature, setTemperature] = useState(15);
     const [uvRadaition, setUvRadaition] = useState(12);
     const [uvBackground, setUvBackground] = useState('bg-purple-800');
 
@@ -18,9 +19,23 @@ function SideBarConditions() {
         }
     }
 
+    const handleConditions = () => {
+        fetch('http://api.weatherapi.com/v1/current.json?key=485dd0ceac38418db2b183840250302&q=Coquimbo&aqi=no')
+            .then(response => response.json())
+            .then(data => {
+                setTemperature(data.current.temp_c);
+                setUvRadaition(data.current.uv);
+            })
+            .catch(error => {
+                console.error('There was an error!', error);
+            });
+    }
+    useEffect(() => {
+        handleConditions();
+    }, []);
+
     useEffect(() => {
         scaleBackground(uvRadaition);
-        setUvRadaition(12);
     }, [uvRadaition]);
 
     return (
@@ -32,12 +47,12 @@ function SideBarConditions() {
                     {/* Radaicion UV */}
                     <div className={` ${uvBackground} w-full h-40 rounded-lg border-2 border-white flex   pt-4 flex-col justify-center`}>
                         <p className="text-white text-2xl font-semibold">Indice UV</p>
-                        <p className="text-[5rem] font-semibold text-white">{uvRadaition}</p>
+                        <p className="text-[4rem] font-semibold text-white">{uvRadaition}</p>
                     </div>
                     {/* temperatura */}
                     <div className="flex flex-col">
                         <p className="text-white text-2xl font-semibold">Temperatura Actual</p>
-                        <p className="text-[5rem] font-semibold text-white">15°</p>
+                        <p className="text-[5rem] font-semibold text-white">{temperature} °C</p>
                     </div>
                 </div>
             </div>
