6f418bfcf56c8b42b501fb1e8ed8fa52b04af484
JuanNilo <juan.nilo@alumnos.ucn.cl>
2025-02-03 14:45:16 -0300

    FIX: Video autoplay

--- src/components/Main/Cam-Section.tsx
+++ src/components/Main/Cam-Section.tsx
@@ -8,16 +8,7 @@ function Historico() {
     return (
         <div className="min-h-[35vh]  md:min-h-[30vh] p-2 md:p-5 rounded-lg md:mb-5   ">
             <main className="flex">
-                {/* <aside className="hidden md:block w-[100%] max-h-[55vh] overflow-hidden rounded-lg">
-                    <iframe
-                    src={`${bigVideo}`}
-                    frameBorder={0}
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                    allowFullScreen
-                    className="  h-[100%] w-[95vh] pointer-events-none rounded-lg "
-                    ></iframe>
-                    </aside> */}
-                <div className="w-[100%] md:w-[100%] mx-2 flex flex items-center gap-2 p overflow-scrol  overflow-x-scroll max-h-[35vh] md:max-h-[55vh]">
+                <div className="w-[100%] md:w-[100%] mx-2 flex items-center gap-2 p overflow-scrol  overflow-x-scroll max-h-[35vh] md:max-h-[55vh]">
                     {
                         videoData.map((video, index) => (
                             <div key={index} className=" w-full flex items-start justify-center"  >
--- src/components/Main/Card-Video.tsx
+++ src/components/Main/Card-Video.tsx
@@ -1,24 +1,21 @@
 interface CardVideoProps {
     link: string;
 }
-function CardVideo({ link }: CardVideoProps) {
 
+function CardVideo({ link }: CardVideoProps) {
     return (
         <div className="bg-slate-100 w-[100%] shadow-md border-[1px] border-gray-300 text-black rounded-lg overflow-hidden">
-            <div className="w-[45vh] h-[25vh] relative hover:cursor-pointer ">
+            <div className="w-[45vh] h-[25vh] relative hover:cursor-pointer">
                 <iframe
-                    src={`${link}?autoplay=1&controls=0&disablekb=1&modestbranding=1&rel=0&showinfo=0`}
+                    src={`${link}?autoplay=1&controls=1`}
                     frameBorder="0"
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                     allowFullScreen
-                    className="w-full h-full pointer-events-none absolute top-0 left-0 "
+                    className="w-full h-full  absolute top-0 left-0"
                 ></iframe>
             </div>
         </div>
     );
 }
 
-
 export default CardVideo;
-
-{/* <div className="w-full  pb-[59.25%] h-[100%] relative overflow-hidden hover:cursor-pointer "> */ }
