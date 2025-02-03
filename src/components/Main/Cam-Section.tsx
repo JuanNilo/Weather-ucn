34809a325e3086ae78b9e7bbb80ce0127d14afaa
JuanNilo <juan.nilo@alumnos.ucn.cl>
2025-02-03 11:10:12 -0300

    UPDATE: Refactor Cam-Section component to use video data from JSON file

--- src/components/Main/Cam-Section.tsx
+++ src/components/Main/Cam-Section.tsx
@@ -1,17 +1,10 @@
 import CardVideo from "./Card-Video"
 import { useState } from "react"
+import videoData from "../../data/videos.json";
 
 function Historico() {
-    const videos = [
-        "https://ucncamcq.ucn.cl:8181/f537af01-e143-49b9-901a-6e6b67be9073.html",
-        "https://ucncamcq.ucn.cl:8181/62e76aea-0f11-4c9f-ac40-3bb6da9ff5dd.html",
-        "https://ucncamcq.ucn.cl:8181/31334bb8-688e-4b31-9024-503a2e88a7b3.html",
-        "https://ucncamcq.ucn.cl:8181/cb57419f-fb54-4a70-ada7-b567d45f75ad.html",
-        "https://ucncamcq.ucn.cl:8181/3074b3e7-e634-482b-8d9a-014bc4b46433.html",
-        "https://ucncamcq.ucn.cl:8181/39eb3ca2-3e0c-49e8-a8b0-6b5e607be974.html"
-    ]
 
-    const [bigVideo, setBigVideo] = useState(videos[0])
+    const [bigVideo, setBigVideo] = useState(videoData[0].url)
 
     return (
         <div className="min-h-[35vh] md:min-h-[60vh] p-2 md:p-5 rounded-lg md:mb-5   ">
@@ -28,9 +21,9 @@ function Historico() {
                 </aside>
                 <div className="w-[100%] md:w-[60%] mx-2 flex flex-col items-center gap-2 p overflow-scrol  overflow-x-hidden max-h-[35vh] md:max-h-[55vh]">
                     {
-                        videos.map((video, index) => (
-                            <div key={index} className=" w-[55vh] flex items-start justify-center" onClick={() => setBigVideo(video)} >
-                                <CardVideo link={video} />
+                        videoData.map((video, index) => (
+                            <div key={index} className=" w-[55vh] flex items-start justify-center" onClick={() => setBigVideo(video.url)} >
+                                <CardVideo link={video.url} />
                             </div>
                         ))
                     }
