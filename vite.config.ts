3dbc2e9c67616c87623e7a7759a89b20b250af87
JuanNilo <juan.nilo@alumnos.ucn.cl>
2025-02-04 10:48:17 -0300

    FiX Certificados v2

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import fs from 'fs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,10 +13,9 @@ export default defineConfig({
     port: 80,
     allowedHosts: ['dismet.ucn.cl'],
     https: {
+      key: fs.readFileSync('./certs/wildcard_ucn_cl.key'),
       //@ts-ignore
-      key: "/Users/juannilo/Downloads/UCN_2024/KEY/wildcard_ucn_cl.key",
-      //@ts-ignore
-      cert: "/Users/juannilo/Downloads/UCN_2024/KEY/wildcard_ucn_cl.cert",
+      cert: fs.readFileSync('./certs/wildcard_ucn_cl.crt'),
     },
     proxy: {
       '/api': {
