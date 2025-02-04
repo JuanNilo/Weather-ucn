daf7b639df911e511736649cc6d4c6d0bfa59974
JuanNilo <juan.nilo@alumnos.ucn.cl>
2025-02-04 15:18:10 -0300

    Responsive

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -1,16 +1,16 @@
 
 function Footer() {
     return (
-        <footer className="bg-[#23415b] text-white h-20 flex justify-around text-sm px-20">
-            <div className="flex items-center  w-[30%] ">
+        <footer className="bg-[#23415b] text-white h-24 md:h-20 flex flex-col md:justify-around text-sm px-20">
+            <div className="flex text-center items-center  w-full md:w-[30%] ">
                 <img src="/public/logoceaza.jpg" alt="Logo" className="h-full hidden md:block p-2" />
                 <p>Datos provistos por CEAZA, obtenidos desde <a href="www.ceazamet.cl">www.ceazamet.cl</a></p>
             </div>
-            <div className="w-[40%] flex flex-col justify-center items-center">
+            <div className="w-full md:w-[40%] flex font-semibold flex-col justify-center items-center">
                 <p>Departamento de Informática - UCN</p>
                 <p>Coquimbo 2025</p>
             </div>
-            <div className="w-[30%] flex justify-end items-center ">
+            <div className="w-[30%] hidden md:flex justify-end items-center ">
                 <img src="/public/Banner.PNG" alt="Logo" className="h-full" />
             </div>
         </footer>
--- src/components/Main/TopBar.tsx
+++ src/components/Main/TopBar.tsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import axios from 'axios';
 
 function TopBar() {
 
@@ -9,15 +10,34 @@ function TopBar() {
             .then(response => response.json())
             .then(data => {
                 console.log(data);
-                setTemperature(data.current.temp_c);
                 setUvRadaition(data.current.uv);
             })
             .catch(error => {
                 console.error('There was an error!', error);
             });
     }
+
+    const fetchConditions = async () => {
+        const today = new Date();
+        today.setUTCHours(0, 0, 0, 0); // Ajustar la hora a 00:00:00.000Z
+        const todayISOString = today.toISOString(); // Obtener la fecha de hoy en formato YYYY-MM-DDTHH:mm:ss.sssZ
+        const response = await axios.get(`${import.meta.env.VITE_API_URL}/api/data/${todayISOString}`);
+
+        // Ordenar los datos por la fecha y la hora
+        const sortedData = response.data.sort((a: any, b: any) => {
+            const dateA = new Date(`${a.fecha}T${a.hora}:00.000Z`);
+            const dateB = new Date(`${b.fecha}T${b.hora}:00.000Z`);
+            return dateA.getTime() - dateB.getTime();
+        });
+
+        console.log("data sorted ✅", sortedData);
+        const latestData = sortedData[sortedData.length - 2];
+
+        setTemperature(latestData.temperatura.toFixed(1));
+    }
+
     useEffect(() => {
-        console.log('fetching Temperatura y Radiacion UV');
+        fetchConditions();
         handleConditions();
     }, []);
 
