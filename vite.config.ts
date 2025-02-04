4a12c9280ae08304505867b3ae9af82fb348711d
JuanNilo <juan.nilo@alumnos.ucn.cl>
2025-02-04 12:05:47 -0300

    Hidden Cert conft

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import fs from 'fs'
+// import fs from 'fs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,11 +12,11 @@ export default defineConfig({
     host: '146.83.129.107',
     port: 80,
     allowedHosts: ['dismet.ucn.cl'],
-    https: {
-      key: fs.readFileSync('./certs/wildcard_ucn_cl.key'),
-      //@ts-ignore
-      cert: fs.readFileSync('./certs/wildcard_ucn_cl.crt'),
-    },
+    // https: {
+    //   key: fs.readFileSync('./certs/wildcard_ucn_cl.key'),
+    //   //@ts-ignore
+    //   cert: fs.readFileSync('./certs/wildcard_ucn_cl.crt'),
+    // },
     proxy: {
       '/api': {
         target: 'https://www.ceazamet.cl',
