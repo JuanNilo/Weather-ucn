ead1b2805df66a5ef775fe3294a52a6e4abf6683
JuanNilo <juan.nilo@alumnos.ucn.cl>
2025-02-04 10:18:17 -0300

    UPDATE: Certificado

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import fs from 'fs'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -11,6 +12,10 @@ export default defineConfig({
     host: '146.83.129.107',
     port: 80,
     allowedHosts: ['dismet.ucn.cl'],
+    https: {
+      key: fs.readFileSync(process.env.KEY_PATH || 'default/path/to/key.pem'),
+      cert: fs.readFileSync(process.env.TZ || 'C:/Users/Usuario/Desktop/ucn-iot/ucn-iot.crt')
+    },
     proxy: {
       '/api': {
         target: 'https://www.ceazamet.cl',
